11da3bf4bdbd8a3fcfd4648ee5a62ae81c655074
mesh1nek0x0 <mesh1nek0x0@gmail.com>
2018-07-08 15:43:29 +0900

    :sparkles: add class for message event

---
+++ src/lib/message_event.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = class MessageEvent {
+    constructor(event) {
+        if (!event.type || event.type !== 'message') {
+            throw new Error('cannot generate message event');
+        }
+        this.type_ = event.type;
+        this.id_ = event.message.id;
+    }
+
+    get type() {
+        return this.type_;
+    }
+
+    get id() {
+        return this.id_;
+    }
+};
